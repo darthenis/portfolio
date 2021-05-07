a28c831d313f5f1807e6a93cc1812af24b0113ca
darthenis <emi.acevedo.letras@gmail.com>
2021-05-06 22:10:32 -0300

    version con el https servidor

--- frontend/src/components/projects/trivia/triviaservice.tsx
+++ frontend/src/components/projects/trivia/triviaservice.tsx
@@ -4,13 +4,13 @@ import {Player} from './trivia-interfaces-types'
 
 export const addPlayer = async (data : Player) =>{
 
-    return await axios.post('https://localhost:4000/addplayer', data)
+    return await axios.post('https://personalwebapi.herokuapp.com/addplayer', data)
 
 }
 
 export const getPlayers = async () =>{
 
-   const data =  await axios.get('https://localhost:4000/getplayers');
+   const data =  await axios.get('https://personalwebapi.herokuapp.com/getplayers');
 
    return data;
 
--- frontend/src/components/rrss-service.ts
+++ frontend/src/components/rrss-service.ts
@@ -6,7 +6,7 @@ export const sendMail = async (message : message) => {
 
     console.log('mensaje enviado: ', message)
 
-    const result = await axios.post('https://localhost:4000/sendmessage', message)
+    const result = await axios.post('https://personalwebapi.herokuapp.com/sendmessage', message)
 
     return result;
 }
