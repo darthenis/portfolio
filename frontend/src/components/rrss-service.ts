03166e4b7cb0abdde169d2f8cabbff25520fbed0
darthenis <emi.acevedo.letras@gmail.com>
2021-05-08 16:35:32 -0300

    probandooo

--- frontend/src/components/rrss-service.ts
+++ frontend/src/components/rrss-service.ts
@@ -6,7 +6,7 @@ export const sendMail = async (message : message) => {
 
     console.log('mensaje enviado: ', message)
 
-    const result = await axios.post('http://personalwebapi.herokuapp.com/sendmessage', message)
+    const result = await axios.post('https://personalwebapi.herokuapp.com/sendmessage', message)
 
     return result;
 }
