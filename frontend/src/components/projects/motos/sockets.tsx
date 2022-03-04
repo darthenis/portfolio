9ade7bf5e392dec88c2792ecac6670f286246dac
darthenis <emi.acevedo.letras@gmail.com>
2022-03-04 12:42:29 -0300

    done

--- frontend/src/components/projects/motos/sockets.tsx
+++ frontend/src/components/projects/motos/sockets.tsx
@@ -1,6 +1,6 @@
 import { io } from 'socket.io-client'
 
-const socket = io('http://personalwebapi.herokuapp.com', {
+const socket = io('https://personalwebapi.herokuapp.com', {
   withCredentials: true,
   extraHeaders: {
     "my-custom-header":"abcd"
