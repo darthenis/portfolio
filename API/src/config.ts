d004556bc55761633f51bf148a738191c421e8b2
darthenis <emi.acevedo.letras@gmail.com>
2022-03-08 12:34:37 -0300

    update 8/3

--- API/src/config.ts
+++ API/src/config.ts
@@ -6,10 +6,10 @@ export default {
      MONGO_USER :  process.env.MONGO_USER || 'admin',
      MONGO_PASS :  process.env.MONGO_PASS || 'lorencia676',
      PORT       :  process.env.PORT || 4000,
-     WEBURL     :  'https://webpersonal-darthenis.vercel.app'
+     WEBURL     :  'https://personalwebapi.herokuapp.com'
 }
 
 
-// dev: http://localhost:3000 
+// dev: http://localhost:3000
 
-// production: https://webpersonal-darthenis.vercel.app
+// production: https://personalwebapi.herokuapp.com
