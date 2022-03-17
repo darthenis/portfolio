fc186ed5932af944d1727a715d184f859c92548c
darthenis <emi.acevedo.letras@gmail.com>
2022-03-17 16:13:21 -0300

    horarios-motos

--- frontend/src/components/projects/motos/motos.tsx
+++ frontend/src/components/projects/motos/motos.tsx
@@ -9,27 +9,8 @@ import {MotosDiv} from './motos-styled'
 
 //---------------------------------HORARIOS-------------------------------------
 
-let horarios: string[]=[];
-
-let minutes=480;
-
-for (let i=0;i<=24;i++){
-
-      let hour : number | string = Math.floor(minutes/60)
-
-      hour = (hour < 10) ? '0' + hour : hour;
-
-      let minute : string | number = minutes % 60;
-
-      minute = (minute < 10) ? '0' + minute : minute;
-
-      let protohorario = hour + ':' + minute
-
-      horarios.push(protohorario +' hs');
-
-      minutes+=30;
-
-    }
+let horarios: string[]=['8:00', '8:30', '9:00', '9:30', '10:00', '10:30', '11:00', '11:30', '12:00', '12:30', '13:00', '13:30', 
+  '14:00', '14:30', '15:00', '15:30', '16:00', '16:30', '17:00', '17:30', '18:00', '18:30', '19:00', '19:30', '20:00'];
 
 //-------------------------------ACTIONUSERS------------------------------------
 
